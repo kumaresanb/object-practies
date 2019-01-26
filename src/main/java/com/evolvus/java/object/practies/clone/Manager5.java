0b30cfa6f68fa50526b2b37b1342f4589ffd484b
Kumaresan BK <EVOLVUS\kumaresanb@ELPBLR-035.evolvus.com>
2019-01-26 23:18:51 +0530

    hashcode and clone

--- src/main/java/com/evolvus/java/object/practies/clone/Manager5.java
+++ src/main/java/com/evolvus/java/object/practies/clone/Manager5.java
@@ -19,6 +19,13 @@ public class Manager5 {
 		E e1 =new E(34);
 		E e2=e1;
 		System.out.println(e2.k);
+		e2.k=200;
+		System.out.println(e1.k);
+		e1.k=300;
+		System.out.println(e1.k);
+		System.out.println(e1==e2);
+		System.out.println(e1.hashCode());
+		System.out.println(e2.hashCode());
 	}
 
 }
